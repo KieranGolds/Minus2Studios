2b7d0c80752511a8088c079740021df628cb1e4f
KieranGolds <Kieranalexstrasza@gmail.com>
2017-01-24 22:04:17 +0000

    Test for unreal project

--- MainGameFile/Unreal Projects/MyProject/Intermediate/Build/Win64/UE4Editor/Development/MyProject/MyProject.h
+++
@@ -1,5 +0,0 @@
-#ifndef __AUTO_MYPROJECT_H__
-#define __AUTO_MYPROJECT_H__
-//Last Write: 24/01/2017 19:48:25 +00:00
-#include "C:\Users\Kieran\Documents\Unreal Projects\MyProject\Source\MyProject\MyProject.h"
-#endif//__AUTO_MYPROJECT_H__
