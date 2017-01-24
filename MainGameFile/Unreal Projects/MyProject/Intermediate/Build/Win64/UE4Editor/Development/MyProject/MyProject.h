cff4553f7273e33aee33c210c630a978a004d755
KieranGolds <Kieranalexstrasza@gmail.com>
2017-01-24 21:30:11 +0000

    Added unreal project file with all models imported

---
+++ MainGameFile/Unreal Projects/MyProject/Intermediate/Build/Win64/UE4Editor/Development/MyProject/MyProject.h
@@ -0,0 +1,5 @@
+#ifndef __AUTO_MYPROJECT_H__
+#define __AUTO_MYPROJECT_H__
+//Last Write: 24/01/2017 19:48:25 +00:00
+#include "C:\Users\Kieran\Documents\Unreal Projects\MyProject\Source\MyProject\MyProject.h"
+#endif//__AUTO_MYPROJECT_H__
---
+++ MainGameFile/Unreal Projects/MyProject/Intermediate/Build/Win64/UE4Editor/Development/MyProject/PCH.MyProject.h.cpp
@@ -0,0 +1,1 @@
+#include "C:\Users\Kieran\Documents\Unreal Projects\MyProject\Source\MyProject\MyProject.h"
